7c7c894ff7d15755ed2666fa7adf51e94bb46fd5
datgtr001 <19473791.dat@student.iuh.edu.vn>
2024-01-12 21:24:36 +0700

    test commit

---
+++ demo/src/main/java/com/example/tuan01_api/ApiWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tuan01_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWithSpringBootApplication.class, args);
+	}
+
+}
